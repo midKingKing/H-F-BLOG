bd3a405c69b590fee4d859a4a0e4b51681ffbd55
Jimi.feng <Jimi.feng@alo7.com>
2019-12-13 16:14:43 +0800

    apply

--- src/main/java/com/hf/config/WebMvcConfig.kt
+++ src/main/java/com/hf/config/WebMvcConfig.kt
@@ -14,15 +14,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableWebMvc
 open class WebMvcConfig : WebMvcConfigurerAdapter() {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.run {
+        super.addResourceHandlers(registry.apply {
             addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/")
             addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
             addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/")
             addResourceHandler("/src/**").addResourceLocations("classpath:/static/src/")
             addResourceHandler("/bootstrap-3.3.7/**").addResourceLocations("classpath:/static/bootstrap-3.3.7/")
             addResourceHandler("/font-awesome-4.6.3/**").addResourceLocations("classpath:/static/font-awesome-4.6.3/")
-        }
-        super.addResourceHandlers(registry)
+        })
     }
 
     override fun addInterceptors(registry: InterceptorRegistry) {
